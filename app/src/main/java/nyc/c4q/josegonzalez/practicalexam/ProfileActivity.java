0b0b09fdb52ebca7b91792f30c1219b0bff3ae7a
joseegonzalez <mathcore@gmail.com>
2017-02-12 14:46:00 -0500

    toasts working for all buttons in profile activity

--- app/src/main/java/nyc/c4q/josegonzalez/practicalexam/ProfileActivity.java
+++ app/src/main/java/nyc/c4q/josegonzalez/practicalexam/ProfileActivity.java
@@ -1,7 +1,9 @@
 package nyc.c4q.josegonzalez.practicalexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+    public void heart(View view){
+        Toast.makeText(getApplicationContext(),
+                "Selected Heart Activity",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    public void mail(View view){
+        Toast.makeText(getApplicationContext(),
+                "Selected Message Activity",
+                Toast.LENGTH_SHORT)
+                .show();
+
+    }
+    public void contacts(View view){
+
+
+    }
+    public void logout(View view){
+        Toast.makeText(getApplicationContext(),
+                "Selected Logout Activity",
+                Toast.LENGTH_SHORT)
+                .show();
+
+
     }
 }
